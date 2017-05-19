257f2f549fe48ebc73d763ce8e84c7f820ea5494
Shuhei Nomura <info@danmaq.com>
2017-05-19 10:46:32 +0900

    Added position class.

--- imports/core/models.js
+++ imports/core/models.js
@@ -74,10 +74,74 @@ class Rate {
         this._tick = tick;
         this._ask = ask;
         this._bid = bid;
+        this._spread = ask - bid;
     }
 
     /** Currency pair. */
     get pair() {
         return this._pair;
     }
+
+    /** Timestamp. */
+    get tick() {
+        return this._tick;
+    }
+
+    /** Ask point. */
+    get ask() {
+        return this._ask;
+    }
+
+    /** Bid point. */
+    get bid() {
+        return this._bid;
+    }
+
+    /** Spread point. */
+    get spread() {
+        return this._spread;
+    }
+
+    /** Get point by exchange type. */
+    getOrderPoint(exType) {
+        return exType === Exchange.Buy ? this.ask : this.bid;
+    }
+
+    /** Get point by exchange type. */
+    getStopPoint(exType) {
+        return exType === Exchange.Buy ? this.bid : this.ask;
+    }
+}
+
+class Position {
+    /**
+     * Initialize new object.
+     * @param rate Rate instance.
+     * @param quantity Ordered quantity.
+     * @param exType Exchange type.
+     */
+    constructor(rate, quantity, exType) {
+        this._rate = rate;
+        this._quantity = quantity;
+        this._exType = exType;
+    }
+
+    /** Rate instance. */
+    get rate() {
+        return this._rate;
+    }
+
+    /** Ordered quantity. */
+    get quantity() {
+        return this._quantity;
+    }
+
+    /** Exchange type. */
+    get exchangeType() {
+        return this._exType;
+    }
+}
+
+class Ordere {
+
 }
