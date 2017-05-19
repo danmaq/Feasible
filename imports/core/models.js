e6fb5948d3de61cc2dc2754d90b9ba23134d89d7
Shuhei Nomura <info@danmaq.com>
2017-05-19 11:04:22 +0900

    Added empty web ui.

--- imports/core/models.js
+++ imports/core/models.js
@@ -142,6 +142,31 @@ class Position {
     }
 }
 
-class Ordere {
+class Order {
+    /**
+     * Initialize new object.
+     * @param rate Rate instance.
+     * @param quantity Ordered quantity.
+     * @param exType Exchange type.
+     */
+    constructor(rate, quantity, exType) {
+        this._rate = rate;
+        this._quantity = quantity;
+        this._exType = exType;
+    }
 
+    /** Rate instance. */
+    get rate() {
+        return this._rate;
+    }
+
+    /** Ordered quantity. */
+    get quantity() {
+        return this._quantity;
+    }
+
+    /** Exchange type. */
+    get exchangeType() {
+        return this._exType;
+    }
 }
