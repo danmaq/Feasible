738fa800780e867405c37b859dd3f1f11c461af2
Shuhei Nomura <info@danmaq.com>
2017-07-20 11:04:30 +0900

    Added page navigate.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,8 +4,9 @@ import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
 import './body.html';
+import './lp.js';
 import './account/accounts.js';
 
-Template.body.onCreated(() => { console.log(Meteor.userId()); });
-Template.body.helpers({ "uid": () => Meteor.userId() });
+Template.body.onCreated(() => {});
+Template.body.helpers({});
 Template.body.events({});
---
+++ imports/ui/lp.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { Template } from 'meteor/templating';
+
+import './lp.html';
+
+Template.lp.onCreated(() => {});
+Template.lp.helpers({});
+Template.lp.events({});
