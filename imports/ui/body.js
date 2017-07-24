2cfeaa3da6cb77ee8cba0a600667ebf0b7e7a0d1
Shuhei Nomura <info@danmaq.com>
2017-07-24 11:03:44 +0900

    Implemented accounts page navigation.

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,5 @@
 'use strict';
 
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-
 import './body.html';
 import './lp.js';
 import './account/accounts.js';
-
-Template.body.onCreated(() => {});
-Template.body.helpers({});
-Template.body.events({});
--- imports/ui/lp.js
+++ imports/ui/lp.js
@@ -1,9 +1,3 @@
 'use strict';
 
-import { Template } from 'meteor/templating';
-
 import './lp.html';
-
-Template.lp.onCreated(() => {});
-Template.lp.helpers({});
-Template.lp.events({});
