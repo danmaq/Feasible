dd3e2c160be68909fe00bd5a62eb80b026fc7476
Shuhei Nomura <info@danmaq.com>
2017-08-03 09:59:14 +0900

    Implemented directions API.

--- imports/api/directions.js
+++ imports/api/directions.js
@@ -8,18 +8,24 @@ import { Operation } from '../core/enums/operation.js';
 
 /** Directions collection. */
 export const Directions = new Mongo.Collection('directions');
+import { Direction } from '../core/models/direction.js';
 
 if (Meteor.isServer) {
     Meteor.publish(
         'directions',
-        () => Directions.find({ owner: Meteor.userId() }));
+        () => Directions.find({ "owner": Meteor.userId() }));
 }
 
 Meteor.methods({
-    "directions.insert": () => {
+    "directions.insert": (operation, positionId, orderId) => {
+        check(operation, Number);
+        check(positionId, String);
+        check(orderId, String);
         if (!Meteor.userId()) {
             throw new Meteor.Error('not-authorized');
         }
+        const dir = new Direction(operation, positionId, orderId);
+        Directions.insert({ "body": dir, "owner": Meteor.userId() });
     },
     "directions.flush": () => {
         if (!Meteor.userId()) {
@@ -27,4 +33,4 @@ Meteor.methods({
         }
         Directions.remove({ owner: Meteor.userId() });
     },
-})
+});
