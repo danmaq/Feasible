758ee95b1fef657a30f8cf5d9465ab8fbccc2a08
Shuhei Nomura <info@danmaq.com>
2017-08-11 16:35:15 +0900

    Replaced sign-in ui using bootstrap-3

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,8 +1,25 @@
 'use strict';
 
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { Tracker } from 'meteor/tracker';
+
 import './body.html';
 import './lp.js';
 import './account/accounts.js';
 import './account/detail.js';
 import './account/modify.js';
 import './account/form.html';
+
+Tracker.autorun(() => {
+    if (!Meteor.userId()) {
+        FlowRouter.go('/');
+    }
+});
+
+Template._loginButtonsAdditionalLoggedInDropdownActions.events({
+    "click #dashboard": event => {
+        event.preventDefault();
+        FlowRouter.go('/account');
+    }
+});
